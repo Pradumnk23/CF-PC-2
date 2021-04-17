5c4310991ddc3332e57250e9bb46aadd17740c2d
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-04-17 15:51:38 +0530

    Create Panoramix's Prediction.cpp

---
+++ Panoramix's Prediction.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <math.h>
+#include<algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+#define mp                  make_pair
+#define pb                  push_back
+#define lli                 long long int
+#define pl                  pair<lli,lli>
+#define pi                  pair<int,int>
+#define vl                  vector<lli>
+#define fastio              ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define bits(x)             __builtin_popcountll(x)
+#define ld                  long double
+#define all(x)              x.begin(),x.end()
+#define fn                  for(lli i=0;i<n;i++)
+#define rep(i,a,b)          for(lli i=a;i<b;i++)
+#define repo(i,a,b)         for(lli i=a;i>=b;i--)
+#define test()              lli test; cin>>test; while(test--)
+#define fi                  first
+#define se                  second
+#define yes                 cout<<"YES"<<endl
+#define no                  cout<<"NO"<<endl
+#define sp(x)               fixed << setprecision(x)
+
+int gcd(int A, int B)
+{
+    if (B == 0)
+        return A;
+    else
+        return gcd(B, A % B);
+}
+
+int main()
+{
+    fastio;
+        lli n,m,i=0;
+        cin>>n>>m;
+        lli a[]={2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,51};
+        while(i<17)
+        {
+            if(a[i]==n)
+            {
+                if(a[i+1]==m){yes;break;}
+                else{no;break;}
+            }
+            i++;
+        }
+    return 0;
+}
