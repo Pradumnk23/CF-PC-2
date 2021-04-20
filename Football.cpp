aecea671d72f3a8844f076baa046fa1aa7c79fd5
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-04-21 00:25:12 +0530

    M-2->L-2

--- Football.cpp
+++ Football.cpp
@@ -61,3 +61,12 @@ int main()
 
     return 0;
 }
+/*
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n;cin>>n;string s[n];
+	for(int i=0;i<n;i++)cin>>s[i];
+	sort(s,s+n);cout<<s[n/2];
+}
+*/
