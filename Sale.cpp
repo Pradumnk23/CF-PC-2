6f6481d596f3d1581f2b07b6cf07413c777c4274
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-04-21 15:17:38 +0530

    sort then add abs(negative no)

---
+++ Sale.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <math.h>
+#include<algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+#define mp                  make_pair
+#define pb                  push_back
+#define lli                 long long int
+#define sl                  set<lli>
+#define pl                  pair<lli,lli>
+#define pi                  pair<int,int>
+#define vl                  vector<lli>
+#define fastio              ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define bits(x)             __builtin_popcountll(x)
+#define ld                  long double
+#define all(x)              x.begin(),x.end()
+#define fn                  for(lli i=0;i<n;i++)
+#define rep(i,a,b)          for(lli i=a;i<b;i++)
+#define repo(i,a,b)         for(lli i=a;i>=b;i--)
+#define test()              lli test; cin>>test; while(test--)
+#define fi                  first
+#define se                  second
+#define yes                 cout<<"YES"<<endl
+#define no                  cout<<"NO"<<endl
+#define sp(x)               fixed << setprecision(x)
+
+int gcd(int A, int B)
+{
+    if (B == 0)
+        return A;
+    else
+        return gcd(B, A % B);
+}
+
+int main()
+{
+    fastio;
+    lli n,m,cnt=0;
+    cin>>n>>m;
+    lli a[n],s=0;
+    fn {cin>>a[i];}
+    sort(a,a+n);
+    fn {if(a[i]<0){s+=abs(a[i]);cnt++;}if(cnt==m){break;} }
+    cout<<s<<endl;
+
+    return 0;
+}
