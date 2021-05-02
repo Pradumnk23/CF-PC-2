446f3d51b65b41b2094f262205a1091ac61985c7
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-05-02 17:29:05 +0530

    if(a[i+k]-a[i]<a[j+k]-a[j])->j=i; print(j+1)

---
+++ Fence.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <math.h>
+#include<algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+#define mp                  make_pair
+#define pb                  push_back
+#define lli                 long long int
+#define pl                  pair<lli,lli>
+#define pi                  pair<int,int>
+#define vl                  vector<lli>
+#define fastio              ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define bits(x)             __builtin_popcountll(x)
+#define ld                  long double
+#define all(x)              x.begin(),x.end()
+#define fn                  for(lli i=0;i<n;i++)
+#define rep(i,a,b)          for(lli i=a;i<b;i++)
+#define repo(i,a,b)         for(lli i=a;i>=b;i--)
+#define test()              lli test; cin>>test; while(test--)
+#define fi                  first
+#define se                  second
+#define yes                 cout<<"YES"<<endl
+#define no                  cout<<"NO"<<endl
+#define sp(x)               fixed << setprecision(x)
+#define MOD                 1000000007
+
+int gcd(int A, int B)
+{
+    if (B == 0)
+        return A;
+    else
+        return gcd(B, A % B);
+}
+
+int main()
+{
+    fastio;
+    int n,k,temp,sum=0,j=0,a[150001];
+	cin>>n>>k;
+	a[0]=0;
+	for(int i=1;i<=n;i++){
+		cin>>temp;
+		sum+=temp;
+		a[i]=sum;
+	}
+	for(int i=1;i<=n-k;i++){
+		if(a[i+k]-a[i]<a[j+k]-a[j]){
+			j=i;
+		}
+	}
+	cout<<j+1;
+    return 0;
+}
