49536fa0a3ef9365a8832c56ba1eb06932069bfd
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-05-04 21:04:25 +0530

    if(a==100) if(ft>0) ->{ft--;tf--;} else ->tf-=3;

---
+++ Cinema Line.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <math.h>
+#include<algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+#define mp                  make_pair
+#define pb                  push_back
+#define lli                 long long int
+#define pl                  pair<lli,lli>
+#define pi                  pair<int,int>
+#define ml                  map<<lli,lli>
+#define vl                  vector<lli>
+#define fastio              ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define bits(x)             __builtin_popcountll(x)
+#define ld                  long double
+#define all(x)              x.begin(),x.end()
+#define fn                  for(lli i=0;i<n;i++)
+#define rep(i,a,b)          for(lli i=a;i<b;i++)
+#define repo(i,a,b)         for(lli i=a;i>=b;i--)
+#define test()              lli test; cin>>test; while(test--)
+#define fi                  first
+#define se                  second
+#define yes                 cout<<"YES"<<endl
+#define no                  cout<<"NO"<<endl
+#define sp(x)               fixed << setprecision(x)
+#define MOD                 1000000007
+
+int gcd(int A, int B)
+{
+    if (B == 0)
+        return A;
+    else
+        return gcd(B, A % B);
+}
+
+int main()
+{
+    fastio;
+    lli n,tf=0,ft=0;
+    cin>>n;
+    lli a[n];
+    fn
+    {
+        cin>>a[i];
+        if(a[i]==25)
+            tf++;
+        else if(a[i]==50)
+        {
+            ft++;
+            tf--;
+        }
+        else
+        {
+            if(ft>0)
+                {ft--;tf--;}
+            else
+                tf-=3;
+        }
+        if(ft<0 || tf<0)
+            {no;return 0;}
+    }
+    yes;
+    return 0;
+}
