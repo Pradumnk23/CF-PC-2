d767ad9eb2475b098baffa67ee8cea036ac4274f
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-05-10 20:07:43 +0530

    if(a.length()<b.length())->a+=s1;same 4 b, ->check

---
+++ String LCM.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <math.h>
+#include<algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+#define mp                  make_pair
+#define pb                  push_back
+#define lli                 long long int
+#define pl                  pair<lli,lli>
+#define pi                  pair<int,int>
+#define ml                  map<<lli,lli>
+#define vl                  vector<lli>
+#define fastio              ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define bits(x)             __builtin_popcountll(x)
+#define ld                  long double
+#define all(x)              x.begin(),x.end()
+#define fn                  for(lli i=0;i<n;i++)
+#define rep(i,a,b)          for(lli i=a;i<b;i++)
+#define repo(i,a,b)         for(lli i=a;i>=b;i--)
+#define test()              lli test; cin>>test; while(test--)
+#define fi                  first
+#define se                  second
+#define yes                 cout<<"YES"<<endl
+#define no                  cout<<"NO"<<endl
+#define sp(x)               fixed << setprecision(x)
+#define MOD                 1000000007
+
+int gcd(int A, int B)
+{
+    if (B == 0)
+        return A;
+    else
+        return gcd(B, A % B);
+}
+
+int main()
+{
+    fastio;
+    test()
+    {
+        string s1,s2;
+        cin>>s1>>s2;
+        string a=s1,b=s2;
+        while(1)
+        {
+            if(a.length()==b.length())
+            {
+                if(a==b)
+                    cout<<a<<endl;
+                else
+                    cout<<-1<<endl;
+                break;
+            }
+            if(a.length()<b.length())
+                a+=s1;
+            else
+                b+=s2;
+
+        }
+
+    }
+    return 0;
+}
