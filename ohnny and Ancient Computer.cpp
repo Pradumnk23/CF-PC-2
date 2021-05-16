185157d42cb50776dd237cacd6e5560e8f85275c
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-05-16 12:33:36 +0530

    if(a>b)->swap(a,b); check if >a*2,4 or 8->cnt++

---
+++ ohnny and Ancient Computer.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <math.h>
+#include<algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+#define mp                  make_pair
+#define pb                  push_back
+#define lli                 long long int
+#define sl                  set<lli>
+#define pl                  pair<lli,lli>
+#define pi                  pair<int,int>
+#define ml                  map<<lli,lli>
+#define vl                  vector<lli>
+#define fastio              ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define bits(x)             __builtin_popcountll(x)
+#define lld                  long double
+#define all(x)              x.begin(),x.end()
+#define fn                  for(lli i=0;i<n;i++)
+#define rep(i,a,b)          for(lli i=a;i<b;i++)
+#define repo(i,a,b)         for(lli i=a;i>=b;i--)
+#define test()              lli test; cin>>test; while(test--)
+#define fi                  first
+#define se                  second
+#define yes                 cout<<"YES"<<endl
+#define no                  cout<<"NO"<<endl
+#define sp(x)               fixed << setprecision(x)
+#define MOD                 1000000007
+
+int gcd(int A, int B)
+{
+    if (B == 0)
+        return A;
+    else
+        return gcd(B, A % B);
+}
+
+int main()
+{
+    fastio;
+    test()
+    {
+        lli a,b,k=0;
+        cin>>a>>b;
+        if(a>b)
+            swap(a,b);
+        while(b>=a*8)a*=8,k++;
+        while(b>=a*4)a*=4,k++;
+        while(b>=a*2)a*=2,k++;
+        cout<<(a==b?k:-1);
+        cout<<endl;
+    }
+    return 0;
+}
