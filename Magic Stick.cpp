47cddad31709ecc0470be61ee534a3e0234322c1
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-05-23 19:56:32 +0530

    if(x>3),x==1 & y==1, y<=3 ->yes;

---
+++ Magic Stick.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <math.h>
+#include<algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+#define mp                  make_pair
+#define pb                  push_back
+#define lli                 long long int
+#define sl                  set<lli>
+#define pl                  pair<lli,lli>
+#define pi                  pair<int,int>
+#define ml                  map<<lli,lli>
+#define vl                  vector<lli>
+#define fastio              ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define bits(x)             __builtin_popcountll(x)
+#define lld                  long double
+#define all(x)              x.begin(),x.end()
+#define fn                  for(lli i=0;i<n;i++)
+#define rep(i,a,b)          for(lli i=a;i<b;i++)
+#define repo(i,a,b)         for(lli i=a;i>=b;i--)
+#define test()              lli test; cin>>test; while(test--)
+#define fi                  first
+#define se                  second
+#define yes                 cout<<"YES"<<endl
+#define no                  cout<<"NO"<<endl
+#define sp(x)               fixed << setprecision(x)
+#define MOD                 1000000007
+
+int main()
+{
+    fastio;
+    test()
+    {
+        lli x,y;
+        cin>>x>>y;
+        if(x>3)
+            yes;
+        else if(x==1)
+            {if(y==1)yes;else{no;}}
+        else if(y<=3)
+            yes;
+        else
+            no;
+    }
+    return 0;
+}
