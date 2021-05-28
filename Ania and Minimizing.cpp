f4b819dff014c158830209dbdf2324b6f38c06ce
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-05-28 19:18:16 +0530

    if(s[i]=='0' && k+1<=n) ->k++;

---
+++ Ania and Minimizing.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <math.h>
+#include<algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+#define mp                  make_pair
+#define pb                  push_back
+#define lli                 long long int
+#define sl                  set<lli>
+#define pl                  pair<lli,lli>
+#define pi                  pair<int,int>
+#define ml                  map<<lli,lli>
+#define vl                  vector<lli>
+#define fastio              ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define bits(x)             __builtin_popcountll(x)
+#define lld                  long double
+#define all(x)              x.begin(),x.end()
+#define fn                  for(lli i=0;i<n;i++)
+#define rep(i,a,b)          for(lli i=a;i<b;i++)
+#define repo(i,a,b)         for(lli i=a;i>=b;i--)
+#define test()              lli test; cin>>test; while(test--)
+#define fi                  first
+#define se                  second
+#define yes                 cout<<"YES"<<endl
+#define no                  cout<<"NO"<<endl
+#define sp(x)               fixed << setprecision(x)
+#define mod                 1000000007
+
+int main()
+{
+    fastio;
+    int n,k;
+    cin>>n>>k;
+    string s;
+    cin>>s;
+    if(n==1 && k>0)
+    {
+        cout<<"0";
+        return 0;
+    }
+    if(s[0]!='1' && k>0)
+    {
+        s[0]='1';
+        k--;
+    }
+    for(lli i=1; i<=k; i++)
+    {
+        if(s[i]=='0' && k+1<=n)
+            k++;
+        else
+            s[i]='0';
+    }
+    cout<<s<<endl;
+}
