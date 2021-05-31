5759c5f188c74bf85787ba04c859069891424d8d
Pradumn Kumar <kumarpradumn2000@gmail.com>
2021-05-31 12:32:52 +0530

    l=0,r=n-1;while(l<r-1&&s[l]==s[r])cnt++; cnt<k->No

---
+++ Split it!.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <math.h>
+#include<algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+#define mp                  make_pair
+#define pb                  push_back
+#define lli                 long long int
+#define sl                  set<lli>
+#define pl                  pair<lli,lli>
+#define pi                  pair<int,int>
+#define ml                  map<<lli,lli>
+#define vl                  vector<lli>
+#define fastio              ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define bits(x)             __builtin_popcountll(x)
+#define lld                  long double
+#define all(x)              x.begin(),x.end()
+#define fn                  for(lli i=0;i<n;i++)
+#define rep(i,a,b)          for(lli i=a;i<b;i++)
+#define repo(i,a,b)         for(lli i=a;i>=b;i--)
+#define test()              lli test; cin>>test; while(test--)
+#define fi                  first
+#define se                  second
+#define yes                 cout<<"YES"<<endl
+#define no                  cout<<"NO"<<endl
+#define sp(x)               fixed << setprecision(x)
+#define mod                 1000000007
+
+int main()
+{
+    fastio;
+    test()
+    {
+        lli n,k;
+        cin>>n>>k;
+        string s;
+        cin>>s;
+        lli l=0,r=n-1,cnt=0;
+        while(l<r-1 && s[l]==s[r])
+        {
+            l++;
+            r--;
+            cnt++;
+        }
+        if(cnt<k)
+            no;
+        else
+            yes;
+    }
+    return 0;
+}
